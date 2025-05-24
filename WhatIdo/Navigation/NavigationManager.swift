96b8a60463f582dcd94f4af4d04e3821bf788abc
Eytsam Elahi <eytsam.elahi555@gmail.com>
2025-05-24 12:41:23 +0500

    Added navigation + added mock stubs for service + added splash

---
+++ WhatIdo/Navigation/NavigationManager.swift
@@ -0,0 +1,32 @@
+//
+//  NavigationManager.swift
+//  WhatIdo
+//
+//  Created by eytsam elahi on 08/05/2025.
+//
+
+import SwiftUI
+
+final class NavigationManager: ObservableObject {
+    @Published var path = NavigationPath()
+
+     func push(screen: Route) {
+        path.append(screen)
+    }
+
+    func pop() {
+        path.removeLast()
+    }
+
+    func popToRoot(index: Int) {
+        path.removeLast( path.count - index)
+    }
+
+    func popViews(_ numberOfViews: Int) {
+        path.removeLast(numberOfViews)
+    }
+}
+
+enum Route: Hashable {
+    case spendings
+}
